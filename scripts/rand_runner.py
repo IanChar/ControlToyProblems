e74dfd8b5ec64eb9fb32d9d10baced60c6a3f833
IanChar <ianchar4@gmail.com>
2019-08-09 10:30:52 -0400

    Added continous version of gyms cart pole.

---
+++ scripts/rand_runner.py
@@ -0,0 +1,52 @@
+"""
+Random Agent that will run any of the toy environments set up.
+Mostly a copy of gym's random agent:
+https://github.com/openai/gym/blob/master/examples/agents/random_agent.py
+"""
+
+import argparse
+import sys
+
+import gym
+from gym import wrappers, logger
+
+import os
+print os.getcwd()
+sys.path.append('..')
+from toy_envs import all_envs
+
+class RandomAgent(object):
+
+        def __init__(self, action_space):
+            self.action_space = action_space
+
+        def act(self, observation, reward, done):
+            return self.action_space.sample()
+
+def make_env(env_name):
+    return all_envs[env_name]()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=None)
+    parser.add_argument('env_name', nargs='?', default='CtsCartPoleEnv',
+                        help='Select environment to run.')
+    args = parser.parse_args()
+
+    logger.set_level(logger.INFO)
+    env = make_env(args.env_name)
+    env.seed(0)
+    agent = RandomAgent(env.action_space)
+
+    episode_count = 10
+    reward = 0
+    done = False
+
+    for i in range(episode_count):
+        ob = env.reset()
+        while True:
+            action = agent.act(ob, reward, done)
+            ob, reward, done, _ = env.step(action)
+            env.render()
+            if done:
+                break
+    env.close()
---
+++ toy_envs/__init__.py
@@ -0,0 +1,5 @@
+from toy_envs.cts_cart_pole import CtsCartPoleEnv
+
+all_envs = {'CtsCartPoleEnv': CtsCartPoleEnv}
+
+__all__ = ['CtsCartPoleEnv']
